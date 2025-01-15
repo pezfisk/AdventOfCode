2a64b96112ad258237028c87b61261f67abfccd3
pez <marcorguezhdez@gmail.com>
2025-01-15 12:58:01 +0100

    Day 2 Part 1 solved

---
+++ day2.py
@@ -0,0 +1,48 @@
+def main():
+    data = open("data/day2_input.txt", "r")
+    
+    safeReports = 0
+
+    for line in data:
+        currentLine = line.strip()
+
+        cutLine = currentLine.split(" ")
+        print(cutLine)
+        
+        i = 0
+        steps = []
+
+        while i < len(cutLine):
+            e = i
+            i += 1
+
+            if i < len(cutLine):
+                step = int(cutLine[e]) - int(cutLine[i])
+
+                if abs(step) < 4 and step != 0:
+                    steps.append(step)
+                else:
+                    i = len(cutLine) + 1
+                    steps.clear()
+        
+        print(steps)
+ 
+        if len(steps) > 0:
+            if steps[0] > 0:
+                positive = True
+            else:
+                positive = False
+    
+            okChecks = 0
+            for num in steps:
+                if (num > 0) == positive:
+                    okChecks += 1
+            
+            if okChecks == len(steps):
+                safeReports += 1
+                print("Safe!")
+
+    print("There are " + str(safeReports) + " safe reports")
+    data.close()
+
+main()
