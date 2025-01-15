290915d9c84d5d6319b02be24ccdd8557ac5ce4e
pez <marcorguezhdez@gmail.com>
2025-01-15 22:28:46 +0100

    Day 3 solved

--- day3.py
+++ day3.py
@@ -1,7 +1,9 @@
 def main():
-    file = open("data/day3_test.txt", 'r')
+    file = open("data/day3_input.txt", "r")
     data = file.read()
-    print(data)
+    # print(data)
+
+    totalMultiplication = 0
 
     i = 0
     while i < len(data) - 3:
@@ -9,13 +11,38 @@ def main():
 
         if test_string == "mul(":
             parCheck = 0
+            print("\n")
             while parCheck < 9:
-                if test_string[i + parCheck] == ")":
+                print(data[i + parCheck + 3])
+                if data[i + parCheck + 3] == ")":
                     print("mul found")
+                    print(parCheck)
+
+                    mulString = ""
+                    mulArray = []
+
+                    e = 0
+                    while e < parCheck - 1:
+                        mulString += str(data[i + parCheck - e + 2])
+                        e += 1
+
+                    charIndex = mulString.find(",")
+                    print(charIndex)
+                    if charIndex != -1:
+                        print("Valid mult found")
+                        mulArray = mulString[::-1].split(",")
+                        print(mulString)
+                        print(mulArray)
+
+                        totalMultiplication += int(mulArray[0]) * int(mulArray[1])
+
+                    parCheck = 10
 
                 parCheck += 1
 
         i += 1
 
+    return totalMultiplication
 
-main()
+
+print(main())
